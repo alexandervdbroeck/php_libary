ae50fd6109eb730fea2741d1ab3a153424ad04b7
alexandervdbroeck <alexander.vdbroeck@gmail.com>
2020-01-08 19:34:07 +0100

    =>datetime update with a program that loops in a agenda day by day using datetime class, get, and database

--- index.php
+++ index.php
@@ -20,10 +20,44 @@ PrintNavBar();
 ?>
 
 <main>
+    <?php
+    // if there is a date put it in a variable for evaluation by datetime class
+    $date = $_GET['dag'];
+    // if there is no date in the url the date of today wil be active
+    $time = (!isset($_GET['dag'])) ? new DateTime("NOW",new DateTimeZone("Europe/Brussels")):new DateTime("$date",new DateTimeZone("Europe/Brussels"));
+    $now = $time->format("Y-m-d");
+    $url = $_SERVER['PHP_SELF']."?dag=";
+    // get data from the server concerning searching on the day
+    $data = GetData("select * from test where tijd like '".$now."%'");
+    // Modify the date for the <a> buttons , next and previous day
+    $back = $time->modify("-1 day")->format("Y-m-d");
+    $up = $time->modify("+2 days")->format("Y-m-d");
+
+    if(count($data)== 0)
+    {
+        echo "<p> today = ".$time->format("l d-m-Y")." there are no tasks</p>";
+    }else {
+        // search for data that is the same as today
+        foreach ($data as $key)
+        {
+            $time = $key['tijd'];
+            $time = new DateTime("$time",new DateTimeZone("Europe/Brussels"));
+            $date = $time->format("l d-m-Y ");
+            $hour = $time->format("h:i a");
+            echo "<p>".$date." om ".$hour." => uw taak => ".$key['activiteit']."</p>";
+        }
+    }
+
+
+
+
+    ?>
+    <a href="<?php echo $url.$back?>">vorige dag<=====</a>
+    <a href="<?php echo $url.$up?>">=====>Volgende dag</a>
 
 </main>
 <aside>
-test
+
 </aside>
 
 <footer>
--- tijd/tijd.php
+++ tijd/tijd.php
@@ -11,7 +11,7 @@ for($i=0;$i<7;$i++)
     $time2 = $time2->modify("next day");
 }
 
-// function that prints a week in a tabel, when giving it a weeknumer (2digit) and a year
+// function that prints a week in a table, when giving it a week number (2digit) and a year
 function PrintWeekfromYearAndWeek($week, $year){
     for( $day=1; $day <= 7; $day++ )
     {
@@ -37,3 +37,39 @@ function PrintWeekfromYearAndWeek($week, $year){
     echo "</table>";
 
 }
+
+//------------scrolling  a agenda day by day, using database , get, and datetime class---------
+
+// if there is a date put it in a variable for evaluation by datetime class
+$date = $_GET['dag'];
+// if there is no date in the url the date of today wil be active
+$time = (!isset($_GET['dag'])) ? new DateTime("NOW",new DateTimeZone("Europe/Brussels")):new DateTime("$date",new DateTimeZone("Europe/Brussels"));
+$now = $time->format("Y-m-d");
+$url = $_SERVER['PHP_SELF']."?dag=";
+// get data from the server concerning searching on the day
+$data = GetData("select * from test where tijd like '".$now."%'");
+// Modify the date for the <a> buttons , next and previous day
+$back = $time->modify("-1 day")->format("Y-m-d");
+$up = $time->modify("+2 days")->format("Y-m-d");
+
+if(count($data)== 0)
+{
+    echo "<p> today = ".$time->format("l d-m-Y")." there are no tasks</p>";
+}else {
+    // search for data that is the same as today
+    foreach ($data as $key)
+    {
+        $time = $key['tijd'];
+        $time = new DateTime("$time",new DateTimeZone("Europe/Brussels"));
+        $date = $time->format("l d-m-Y ");
+        $hour = $time->format("h:i a");
+        echo "<p>".$date." om ".$hour." => uw taak => ".$key['activiteit']."</p>";
+    }
+}
+
+
+
+
+?>
+<a href="<?php echo $url.$back?>">vorige dag<=====</a>
+<a href="<?php echo $url.$up?>">=====>Volgende dag</a>
