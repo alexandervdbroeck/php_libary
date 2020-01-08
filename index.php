5f3f29f769cc72be214352b01dc12a3922e30721
alexandervdbroeck <alexander.vdbroeck@gmail.com>
2020-01-08 10:05:09 +0100

    => fixed a sql statement bug, and the deletion of unneaded files

--- index.php
+++ index.php
@@ -1,31 +1,6 @@
 <?php
 include_once "lib/lib.php";
 
-function PrintNavBar()
-{
-    //navbar items ophalen
-
-    $data = GetData("select * from menu order by men_order");
-
-    // welke webpagina is actief
-    $filePath = basename($_SERVER['SCRIPT_NAME']);
-    $items_temp = LoadTemplate('nav_items');
-    // nav bar items samenstellen
-    foreach ( $data as $row => $value )
-    {
-
-        if($data[$row]['nav_path'] == $filePath){
-            $data[$row]['active'] = "active";
-        }else{
-            $data[$row]['active'] = "";
-        }
-
-    }
-    $items = ReplaceContent($data,$items_temp);
-    $temp = LoadTemplate('nav');
-    print str_replace("@@navitems@@",$items,$temp);
-}
-
 ?>
 <!DOCTYPE html>
 <html>
--- navigatie/navbar.php
+++ navigatie/navbar.php
@@ -2,22 +2,18 @@
 function PrintNavBar()
 {
     //navbar items ophalen
-
-    $data = GetData("select * from navigation order by nav_order ");
-
+    $data = GetData("select * from menu order by men_order");
     // welke webpagina is actief
     $filePath = basename($_SERVER['SCRIPT_NAME']);
     $items_temp = LoadTemplate('nav_items');
     // nav bar items samenstellen
     foreach ( $data as $row => $value )
     {
-
         if($data[$row]['nav_path'] == $filePath){
             $data[$row]['active'] = "active";
         }else{
             $data[$row]['active'] = "";
         }
-
     }
     $items = ReplaceContent($data,$items_temp);
     $temp = LoadTemplate('nav');
